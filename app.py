c225d4a6c537268b9dda4073575b71bfc6e6bb74
wainesville <wainesville@gmail.com>
2024-07-08 23:33:58 -0700

    flask-cupcakes

---
+++ app.py
@@ -0,0 +1,60 @@
+"""Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.app_context().push()
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "sweet_treat"
+
+connect_db(app)
+
+@app.route('/')
+def index_page():
+    cupcakes = Cupcake.query.all()
+    return render_template('index.html', cupcakes = cupcakes)
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def find_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes', methods = ["POST"])
+def create_cupcake():
+    new_cupcake = Cupcake(flavor=request.json['flavor'],size=request.json['size'], rating = request.json['rating'], image= request.json['image'])
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return(response_json, 201)
+    
+@app.route('/api/cupcakes/<int:id>', methods=["PATCH"])
+def update_cupcakes(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake = cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:id>', methods= ["DELETE"])
+def remove_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message="deleted")
+
+
+
+
+
